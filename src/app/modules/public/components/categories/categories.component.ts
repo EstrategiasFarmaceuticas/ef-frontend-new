42f9da267304c55dad5fb6d6c5e8a5cffab03e79
Juan Camilo Maldonado Pacheco <124261727+Griego1606@users.noreply.github.com>
2025-04-12 23:13:05 -0500

    Removes commented-out category data
    Removes the commented-out section of category data. This cleans up the code and reduces unnecessary visual clutter. The original array of categories is still present and functional.

--- src/app/modules/public/components/categories/categories.component.ts
+++ src/app/modules/public/components/categories/categories.component.ts
@@ -18,15 +18,6 @@ export class CategoriesComponent implements AfterViewInit {
     new Category('Diabetes','/diabetes','categoriaKiks.jpg', true, new Date()),
     new Category('Cancer', '/cancer','categoriaOncologic.jpg', true, new Date()),
 
-
-   /*
-    { nombre: 'Kids', imagen: 'categoriaKiks.jpg', ruta: '/bowel-management' },
-    { nombre: 'Oncology Care', imagen: 'categoriaOncologic.jpg', ruta: '/oncology-care' },
-    { nombre: 'Renal Disease', imagen: 'assets/renal-disease.png', ruta: '/renal-disease' },
-    { nombre: 'Protein & Calorie Malnutrition', imagen: 'assets/protein-calorie.png', ruta: '/protein-calorie' },
-    { nombre: 'Dysphagia', imagen: 'assets/dysphagia.png', ruta: '/dysphagia' },
-    { nombre: 'Urinary Tract Infections', imagen: 'assets/urinary-tract.png', ruta: '/urinary-tract' },
-    { nombre: 'Wound Care', imagen: 'assets/wound-care.png', ruta: '/wound-care' },*/
   ];
 
   ngAfterViewInit() {
