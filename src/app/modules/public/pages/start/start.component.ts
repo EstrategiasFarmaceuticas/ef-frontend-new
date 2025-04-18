a63deb5836190e3093b28efdaa72d402f0673c51
Juan Camilo Maldonado Pacheco <124261727+Griego1606@users.noreply.github.com>
2025-04-17 21:29:24 -0500

    Fix image delete

--- src/app/modules/public/pages/start/start.component.ts
+++ src/app/modules/public/pages/start/start.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {CategoriesComponent} from '@module/public/components/categories/categories.component';
 import {BestproductComponent} from '@module/public/components/bestproduct/bestproduct.component';
 import {DistributorsComponent} from '@module/public/components/distributors/distributors.component';
-import {SliderblogComponent} from '@module/public/components/sliderblog/sliderblog.component';
 import {ArticleCarouselComponent} from '@module/public/components/blog/article-carousel/article-carousel.component';
 
 @Component({
@@ -12,7 +11,6 @@ import {ArticleCarouselComponent} from '@module/public/components/blog/article-c
     CategoriesComponent,
     BestproductComponent,
     DistributorsComponent,
-    SliderblogComponent,
     ArticleCarouselComponent,
   ],
   templateUrl: './start.component.html',
