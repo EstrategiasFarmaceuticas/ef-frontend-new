8403c38d207e563e3ea791d36f2a7531e1327a5a
Juan Camilo Maldonado Pacheco <sr.rojo1606@gmail.com>
2026-06-23 23:21:47 -0500

    fix(ssr): use exact hostnames in CommonEngine allowedHosts

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const indexHtml = join(serverDistFolder, 'index.server.html');
 
 const app = express();
 const commonEngine = new CommonEngine({
-  allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', '.estrategiasfarmaceuticas.com', 'ef.srv5.xyz'],
+  allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'estrategiasfarmaceuticas.com', 'www.estrategiasfarmaceuticas.com', 'ef.srv5.xyz'],
 });
 
 /**
